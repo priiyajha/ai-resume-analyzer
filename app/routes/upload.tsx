07acc6a91c36f41624f4fee821a00f619d31e15b
priiyajha <jhapriiyaa2104@gmail.com>
2025-09-29 23:28:17 +0530

    adding upload page

---
+++ app/routes/upload.tsx
@@ -0,0 +1,18 @@
+import  {useState} from 'react'
+import Navbar from "~/components/Navbar";
+
+const Upload = () => {
+    const[isProcessing, setIsProcessing] = useState(false);
+    return (
+    <main className="bg-[url('/images/bg-main.svg)] bg-cover">
+        <Navbar />
+        <section className="main-section">
+            <div className="page-heading">
+                <h1>Smart feedback for your dream job</h1>
+            </div>
+        </section>
+    </main>
+
+    )
+}
+export default Upload
